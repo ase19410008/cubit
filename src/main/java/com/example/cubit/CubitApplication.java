c2947f3a1fa192b3b227b07841aa38235b184c74
AzureAD\佐久間雄大(WiZ) <ase19410008@wiz.ac.jp>
2021-11-27 10:53:34 +0900

    init java11 ver

---
+++ src/main/java/com/example/cubit/CubitApplication.java
@@ -0,0 +1,13 @@
+package com.example.cubit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubitApplication.class, args);
+	}
+
+}
